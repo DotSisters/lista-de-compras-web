4970b5b088efc24330af0674e53abe626265e9f3
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-06-03 17:51:53 -0300

    Adicionada estrutura inicial do projeto

---
+++ ListaDeComprasWeb.WebApp/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração de Serviços
+builder.Services.AddControllersWithViews().AddRazorOptions(options =>
+{
+    // Resetar a configuração padrão do MVC
+    options.ViewLocationFormats.Clear();
+
+    // Views dos módulos: /ModuloCategorias/Apresentacao/Views/Listar.cshtml
+    options.ViewLocationFormats.Add("/Modulo{1}/Apresentacao/Views/{0}.cshtml");
+
+    // Views compartilhadas: /Compartilhado/Apresentacao/Views/_Layout.cshtml
+    options.ViewLocationFormats.Add("/Compartilhado/Apresentacao/Views/{0}.cshtml");
+});
+
+var app = builder.Build();
+
+// Configuração de Middlewares
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+// Execução do App
+app.Run();
